9152d47acc3daf9351ef330e77a74657796a7a6c
Nigel Morris <nigel@pixelagent.co.uk>
2024-01-24 10:12:26 +0000

    update

--- Assets/Scripts/GameOverFade.cs
+++ Assets/Scripts/GameOverFade.cs
@@ -13,22 +13,22 @@ public class GameOverFade : MonoBehaviour
         [Header("References")]
 
         [SerializeField, Tooltip("The canvas group that contains the fade image.")]
-        CanvasGroup m_CanvasGroup = default;
+        public CanvasGroup m_CanvasGroup = default;
 
         [Header("Fade")]
 
         [SerializeField, Tooltip("The delay in seconds before fading starts when winning.")]
-        float m_WinDelay = 4.0f;
+        public float m_WinDelay = 4.0f;
 
         [SerializeField, Tooltip("The delay in seconds before fading starts when losing.")]
-        float m_LoseDelay = 2.0f;
+        public float m_LoseDelay = 2.0f;
 
         [SerializeField, Tooltip("The duration in seconds of the fade.")]
-        float m_Duration = 1.0f;
+        public float m_Duration = 1.0f;
 
         float m_Time;
         bool m_GameOver;
-        bool m_Won;
+        public bool m_Won;
 
         void Start()
         {
@@ -48,6 +48,9 @@ public class GameOverFade : MonoBehaviour
                 else
                     m_CanvasGroup.alpha = Mathf.Clamp01((m_Time - m_LoseDelay) / m_Duration);
             }
+
+
+
         }
 
         void OnGameOver(GameOverEvent evt)
@@ -60,11 +63,22 @@ public class GameOverFade : MonoBehaviour
             }
         }
 
-  
-
-    void OnDestroy()
+        // Event
+        public void OnGameOverEvent()
         {
-            EventManager.RemoveListener<GameOverEvent>(OnGameOver);
+
+            m_CanvasGroup.gameObject.SetActive(true);
+
+            m_GameOver = true;
+
         }
-    }
+
+        
+
+
+        void OnDestroy()
+            {
+                EventManager.RemoveListener<GameOverEvent>(OnGameOver);
+            }
+}
 
